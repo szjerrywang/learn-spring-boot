d091ba3cb5929ce83d1e685a601d7981cd161e25
Jerry Wang <jerry.wang@alphaliongroup.com>
2019-09-07 14:13:08 +0800

    module

---
+++ test-base-srv/src/main/java/club/leopardtech/learn/srv/basic/BasicSrvApplication.java
@@ -0,0 +1,12 @@
+package club.leopardtech.learn.srv.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSrvApplication.class);
+    }
+}
