56be88b6bd86a85be99931d5ee83d9e4f040835c
Jerry Wang <jerry.wang@alphaliongroup.com>
2019-09-07 15:29:44 +0800

    add a log.info

--- test-base-srv/src/main/java/club/leopardtech/learn/srv/basic/BasicSrvApplication.java
+++ test-base-srv/src/main/java/club/leopardtech/learn/srv/basic/BasicSrvApplication.java
@@ -1,12 +1,23 @@
 package club.leopardtech.learn.srv.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicSrvApplication {
+public class BasicSrvApplication implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BasicSrvApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(BasicSrvApplication.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("Application started...");
+
     }
 }
